3b32146b0469182359566f172aeeb246662b0283
jan.srnicek <jan.srnicek@globallogic.com>
2018-07-24 17:27:07 +0200

    Remote jre scripts

--- myths/memory-consumption/java-8/src/main/resources/scripts/runBaseJre.cmd
+++
@@ -1,1 +0,0 @@
-start "Java 8 memory benchmark - Base" "%JRE8_HOME%\bin\java" -jar ..\..\..\..\target\memory-benchmark.jar
--- myths/memory-consumption/java-8/src/main/resources/scripts/runMinimalJre.cmd
+++
@@ -1,1 +0,0 @@
-start "Java 8 memory benchmark - Minimal" "%JRE8_HOME%\bin\java" -jar -Xms1m -Xmx2m  -XX:MaxMetaspaceSize=9m -XX:+UseSerialGC -XX:+UseStringDeduplication  ..\..\..\..\target\memory-benchmark.jar
